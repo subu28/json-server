3677f3bedce2cdbd5f28c456133eec21c2b57b7c
Subhash <subhash@boomerangcommerce.com>
2018-10-16 20:53:00 +0530

    async thing fixed

--- src/server/router/index.js
+++ src/server/router/index.js
@@ -31,67 +31,79 @@ module.exports = (source, opts = { foreignKeySuffix: 'Id' }) => {
     db = low(source, { storage: fileAsync })
   }
 
-  validateData(db.getState())
+  var remaining = function () {
+    validateData(db.getState())
 
-  // Add lodash-id methods to db
-  db._.mixin(lodashId)
-
-  // Add specific mixins
-  db._.mixin(mixins)
-
-  // Expose database
-  router.db = db
-
-  // Expose render
-  router.render = (req, res) => {
-    res.jsonp(res.locals.data)
+    // Add lodash-id methods to db
+    db._.mixin(lodashId)
+  
+    // Add specific mixins
+    db._.mixin(mixins)
+  
+    // Expose database
+    router.db = db
+  
+    // Expose render
+    router.render = (req, res) => {
+      res.jsonp(res.locals.data)
+    }
+  
+    // GET /db
+    router.get('/db', (req, res) => {
+      res.jsonp(db.getState())
+    })
+  
+    // Handle /:parent/:parentId/:resource
+    router.use(nested(opts))
+  
+    // Create routes
+    db.forEach((value, key) => {
+      if (_.isPlainObject(value)) {
+        router.use(`/${key}`, singular(db, key))
+        return
+      }
+  
+      if (_.isArray(value)) {
+        router.use(`/${key}`, plural(db, key, opts))
+        return
+      }
+  
+      var sourceMessage = ''
+      if (!_.isObject(source)) {
+        sourceMessage = `in ${source}`
+      }
+  
+      const msg =
+        `Type of "${key}" (${typeof value}) ${sourceMessage} is not supported. ` +
+        `Use objects or arrays of objects.`
+  
+      throw new Error(msg)
+    }).value()
+  
+    router.use((req, res) => {
+      if (!res.locals.data) {
+        res.status(404)
+        res.locals.data = {}
+      }
+  
+      router.render(req, res)
+    })
+  
+    router.use((err, req, res, next) => {
+      console.error(err.stack)
+      res.status(500).send(err.stack)
+    })
   }
-
-  // GET /db
-  router.get('/db', (req, res) => {
-    res.jsonp(db.getState())
-  })
-
-  // Handle /:parent/:parentId/:resource
-  router.use(nested(opts))
-
-  // Create routes
-  db.forEach((value, key) => {
-    if (_.isPlainObject(value)) {
-      router.use(`/${key}`, singular(db, key))
-      return
-    }
-
-    if (_.isArray(value)) {
-      router.use(`/${key}`, plural(db, key, opts))
-      return
-    }
-
-    var sourceMessage = ''
-    if (!_.isObject(source)) {
-      sourceMessage = `in ${source}`
-    }
-
-    const msg =
-      `Type of "${key}" (${typeof value}) ${sourceMessage} is not supported. ` +
-      `Use objects or arrays of objects.`
-
-    throw new Error(msg)
-  }).value()
-
-  router.use((req, res) => {
-    if (!res.locals.data) {
-      res.status(404)
-      res.locals.data = {}
-    }
-
-    router.render(req, res)
-  })
-
-  router.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send(err.stack)
-  })
-
+  if (db.getState) {
+    remaining();
+  } else {
+    db.then(result => {
+      db = result;
+      remaining();
+    }, error => {
+      db = error;
+      remaining();
+    })
+  }
   return router
 }
