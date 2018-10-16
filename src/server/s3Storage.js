6754098073f861c0c422404c3baf46d2f5681765
Subhash <subhash@boomerangcommerce.com>
2018-10-16 20:16:12 +0530

    fixes

--- src/server/s3Storage.js
+++ src/server/s3Storage.js
@@ -1,44 +1,44 @@
-'use strict';
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
+'use strict'
+var AWS = require('aws-sdk')
+var s3 = new AWS.S3()
 
 module.exports = {
-  read: function (source) {
-    var temp = source.replace(/^s3:\/\//, '').split('/');
-    var bucket = temp.splice(0, 1);
-    var key = temp.join('/');
+  read: function(source) {
+    var temp = source.replace(/^s3:\/\//, '').split('/')
+    var bucket = temp.splice(0, 1)
+    var key = temp.join('/')
     var params = {
       Bucket: bucket,
       Key: key
-    };
-    return new Promise ((resolve, reject) => {
+    }
+    return new Promise((resolve, reject) => {
       s3.getObject(params, (err, data) => {
         if (err) {
-          reject(err);
+          reject(err)
         } else {
-          resolve(JSON.parse(data.Body));
+          resolve(JSON.parse(data.Body))
         }
-      });
+      })
     })
   },
-  write: function (dest, obj) {
-    var temp = dest.replace(/^s3:\/\//, '').split('/');
-    var bucket = temp.splice(0, 1);
-    var key = temp.join('/');
+  write: function(dest, obj) {
+    var temp = dest.replace(/^s3:\/\//, '').split('/')
+    var bucket = temp.splice(0, 1)
+    var key = temp.join('/')
     var params = {
       Bucket: bucket,
       Key: key,
       Body: JSON.stringify(obj),
-      ContentType: "application/json"
-    };
+      ContentType: 'application/json'
+    }
     return new Promise(function (resolve, reject) {
       s3.getObject(params, (err, data) => {
         if (err) {
-          reject(err);
+          reject(err)
         } else {
-          resolve();
+          resolve()
         }
-      });
-    });
+      })
+    })
   }
-};
+}
